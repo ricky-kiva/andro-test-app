8c9bcb74135723fe6b17a3d8f839cf495d3ea3c0
ricky-kiva <ricky.kiva@gmail.com>
2023-03-27 09:13:19 +0700

    add summary

--- app/src/main/java/com/rickyslash/unittestapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/unittestapp/MainActivity.kt
@@ -79,3 +79,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         activityMainBinding.btnSave.visibility = View.VISIBLE
     }
 }
+
+// Testing is a way to test an app, to know whether the app is easy to maintenance in the future
+
+// Testing has 2 intention:
+// - to get information about the app quality
+// - to ensure the app is working as it's needed
+
+// Zero defects: a term that describes that the product result meet the determined specification
+
+// User Acceptence Criteria: list of specification that is expected from user, organized from it's importance level, also whether if it's 'nice to have'
+// User Acceptance Testing: A document contains a list of User Acceptance Criteria item that is approved from developer & engineer
+
+// Test Code: code to do testing process, ensuring the app is running as it's needed. There's 2 kind of Test Code:
+// - Unit Test: a code that tests an individual component/unit from an app (either it's a class, method, or module)
+// --- It works as isolating part of program and give validation whether that component runs well
+// --- It's commonly written in: module-name/src/test/java
+// --- It runs on local JVM & don't have acess to Android Framework API
+
+// - UI Test: A mechanism where an app is going to be tested as the user interact with the app
+// --- Example: making scenario that is going to be tested to the user (input data, press button, swipe screen etc)
+// --- It's commonly written in: module-name/src/androidTest/Java
+// --- It needs device/emulator and Android framework API / other API like Android Testing Support Library
+
+// JUnit is the default library to Test in Android Project
+
+// add function to Test case (Android Studio): SHIFT + CTRL + T
+// @Test annotation let JUnit know that the function is a test case
+// assertion like 'assertEquals' should be added inside the function test case, to assert the desired result & actual result
+// --- assertEquals("Wed, 28 Feb 2018", Utils.toSimpleString(date))
